87b0217f66003dd8fc44dfc5468733780521d67b
astrocket <leonardo.lee1993@gmail.com>
2019-07-28 22:00:25 +0900

    app/* setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def stc
+    "#{controller_path.gsub('_', '-').gsub('/', '--')}--#{action_name}"
+  end
+  
+  def human_time(datetime)
+    datetime.strftime("%m/%d %H:%M")
+  end
 end
